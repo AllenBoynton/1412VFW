3f7cd7bcb88b7588fdf491fcd4cdd3de198986e5
Allen Boynton <aboynton@fullsail.edu>
2014-12-06 19:34:41 -0500

    Adding project 2 to repo.
    Adding for the 4th time.

---
+++ Boynton_Allen_Project2/Resources/app.js
@@ -0,0 +1,15 @@
+// Main .js bootstrap file for cruise ship theme
+
+// Author: Allen Boynton
+// Visual Frameworks 1412
+// Project 2
+// Due: December 6, 2014
+
+Ti.UI.setBackgroundColor("#000");
+
+// Main background
+var mainBackground = Ti.UI.createWindow({
+	backgroundColor: "#fff"
+	//backgroundImage: "Cruiseship.jpg"
+});
+
