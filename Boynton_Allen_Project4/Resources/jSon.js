11364416c14189d2f0772313ef48ffd55b32a5bb
Allen Boynton <aboynton@fullsail.edu>
2014-12-20 20:17:49 -0500

    Added json.js file.
    Added dog breed json file.

---
+++ Boynton_Allen_Project4/Resources/jSon.js
@@ -0,0 +1,25 @@
+var dogBreeds = {
+	"dogs":{
+		"breeds": [
+			{
+				"title"    : "Female Pit bull",
+				"features": "A"
+			},
+			{
+				"title"    : "Golden Retriever",
+				"features": "b"
+			},
+			{
+				"title"    : "Boxer",
+				"features": "C"
+			},
+			{
+				"title"    : "Blue-Nose Pit bull",
+				"features" : "d"
+			},
+			{
+				"title"    : "Minature Pinscher",
+				"features" : "e"
+			}		
+		]
+	},
