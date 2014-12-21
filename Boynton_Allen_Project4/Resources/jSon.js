6558a5d9fb897c3ee182be4ae1b74fb8469c978b
Allen Boynton <aboynton@fullsail.edu>
2014-12-20 23:51:07 -0500

    Changed json data

---
+++ Boynton_Allen_Project4/Resources/jSon.js
@@ -0,0 +1,40 @@
+var dogBreeds = [
+		{
+			"title"    : "Female Pit bull",
+			"Training" : "65",
+			"Strength" : "90",
+			"Description": "A",
+			"Images": "images/Faith.jpg"
+		},
+		{
+			"title"    : "Golden Retriever",
+			"Training" : "90",
+			"Strength" : "70",
+			"Description": "A",
+			"Images": "images/Bandit.jpg"
+		},
+		{
+			"title"    : "Boxer",
+			"Training" : "80",
+			"Strength" : "75",
+			"Description": "A",
+			"Images": "images/Duke.jpg"
+		},
+		{
+			"title"    : "Blue-Nose Pit bull",
+			"Training" : "65",
+			"Strength" : "92",
+			"Description": "A",
+			"Images": "images/Kennah.jpeg"
+		},
+		{
+			"title"    : "Minature Pinscher",
+			"Training" : "70",
+			"Strength" : "45",
+			"Description": "A",
+			"Images": "images/Diego.jpg"
+		}		
+];
+
+//var aboutWindow
+
